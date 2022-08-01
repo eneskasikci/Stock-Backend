d9c8704cec7ceb2d4ab554600eb4c175b5994749
Ali Armağan Uygun <75046911+aliarmgnuygun@users.noreply.github.com>
2022-08-01 13:36:57 +0300

    Dev (#14)
    * ProductController class'ında request işlemleri üzerinde Loglama işlemi yaptım.
    
    * ProductController class'ında request işlemleri üzerinde Loglama işlemi yaptım.

--- src/main/java/tr/edu/anadolu/productlistapp/controller/ProductController.java
+++ src/main/java/tr/edu/anadolu/productlistapp/controller/ProductController.java
@@ -3,6 +3,8 @@ package tr.edu.anadolu.productlistapp.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tr.edu.anadolu.productlistapp.model.Product;
@@ -20,6 +22,8 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
+
+    private final Logger logger = LoggerFactory.getLogger(ProductController.class);
 
 
     @GetMapping("/sortByNameAsc")
@@ -68,36 +72,42 @@ public class ProductController {
     @ApiOperation(value = "Listing every product inside the database.")
     public ResponseEntity<List<Product>> findAll(@RequestParam(defaultValue = "0") int page,
                                                  @RequestParam(defaultValue = "5") int size) {
+        logger.debug("Product listing process completed.");
         return productService.findAll(page, size);
     }
 
     @PostMapping("/createProduct")
     @ApiOperation(value = "Creates a new product.")
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        logger.debug("A new product has been created: " + product.getProductId());
         return productService.createNewProduct(product);
     }
 
     @GetMapping("/{productId}")
     @ApiOperation(value = "Getting a productId from user and returns if it is in database.")
     public Product getOneProduct(@PathVariable @ApiParam(value = "ID of the product") String productId) {
+        logger.debug("The product with id : " + productId + " has been retrieved.");
         return productService.getProduct(productId);
     }
 
     @PutMapping("/updateProduct/{productId}")
     @ApiOperation(value = "Given it is productId, updates the product in the database.")
     public Product updateProduct(@PathVariable @ApiParam(value = "ID of the product") String productId, @RequestBody Product newProduct) {
+        logger.debug("The information of the product with the id: " + productId + " has been updated.");
         return productService.updateProduct(productId, newProduct);
     }
 
     @DeleteMapping("/deleteProduct/{productId}")
     @ApiOperation(value = "Given it is productId, deletes the product in the database.")
     public void deleteProduct(@PathVariable @ApiParam(value = "ID of the product") String productId) {
+        logger.warn("Deleting product with id: " + productId);
         productService.deleteProduct(productId);
     }
 
     @PutMapping("/{productId}/availability")
     @ApiOperation(value = "Sets the product availability if it can be sold or not.", notes = "After calling, the availability of the product changes to true or false.")
     public Product setAvailability(@PathVariable @ApiParam(value = "ID of the product") String productId) {
+        logger.debug("The stock status of the product with : " + productId + " id number has been updated: " + productService.getProduct(productId).isAvailability());
         return productService.setAvailability(productId);
     }
 
@@ -105,12 +115,14 @@ public class ProductController {
     @ApiOperation(value = "Deletes every product in the database.", notes = "There shouldn't be an option like this but it is there anyways. Can be removed.")
     public void deleteAll() {
         productService.deleteAll();
+        logger.warn("All product records in the database have been deleted.");
     }
 
     @GetMapping("/listAvailableProducts")
     @ApiOperation(value = "Lists available products.")
     public ResponseEntity<List<Product>> findAvailableProducts(@RequestParam(defaultValue = "0") int page,
                                                  @RequestParam(defaultValue = "5") int size) {
+        logger.debug("The products in supply are listed.");
         return productService.findAvailableProducts(page, size);
     }
 
@@ -118,6 +130,7 @@ public class ProductController {
     @ApiOperation(value = "Lists not available products.")
     public ResponseEntity<List<Product>> findNotAvailableProducts(@RequestParam(defaultValue = "0") int page,
                                                                @RequestParam(defaultValue = "5") int size) {
+        logger.debug("Unavailable products are listed.");
         return productService.findNotAvailableProducts(page, size);
     }
 }
